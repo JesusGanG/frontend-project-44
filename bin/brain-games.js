cff241e676ab7c8eb3abaa0f572a5411754de1be
Almosov <vip.almosov@mail.ru>
2023-03-21 22:02:01 +0300

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-// eslint-disable-next-line import/extensions
-import brainGames from '../src/cli.js';
+import greetingYou from '../src/cli.js';
 
-brainGames();
+greetingYou();
